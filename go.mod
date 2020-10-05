e999dc76aaadf7ee86a06fe7009266439e1bbf20
Nicholas Altunian <naaltunian@icloud.com>
2020-10-05 18:44:35 -0400

    connect to db

--- go.mod
+++ go.mod
@@ -4,5 +4,6 @@ go 1.13
 
 require (
 	github.com/gorilla/mux v1.8.0
+	github.com/lib/pq v1.8.0 // indirect
 	github.com/prometheus/common v0.14.0
 )
--- main.go
+++ main.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"database/sql"
+	"fmt"
 	"log"
 	"net/http"
 
 	"github.com/gorilla/mux"
+	_ "github.com/lib/pq"
 )
 
 type User struct {
@@ -21,7 +24,20 @@ type Error struct {
 	Message string `json:"message"`
 }
 
+const (
+	host     = "localhost"
+	port     = 5432
+	user     = "postgres"
+	password = "admin"
+	dbname   = "go-jwt"
+)
+
+var db *sql.DB
+
 func main() {
+
+	db = connectToDB()
+	defer db.Close()
 
 	router := mux.NewRouter()
 
@@ -31,6 +47,25 @@ func main() {
 
 	log.Println("Starting server on port 8080")
 	log.Fatal(http.ListenAndServe(":8080", router))
+}
+
+func connectToDB() *sql.DB {
+	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+
+		"password=%s dbname=%s sslmode=disable",
+		host, port, user, password, dbname)
+
+	db, err := sql.Open("postgres", psqlInfo)
+	if err != nil {
+		log.Println("Error connecting to db:", err)
+	}
+
+	err = db.Ping()
+	if err != nil {
+		log.Fatal("Error pinging DB", err)
+	}
+
+	log.Println("Connected to DB")
+	return db
 }
 
 func signup(w http.ResponseWriter, r *http.Request) {

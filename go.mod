84ba4ccce5fa768de4cdfac47e852583d21feac4
Nicholas Altunian <naaltunian@icloud.com>
2020-10-05 15:29:26 -0400

    Add base functions

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/naaltunian/go-jwt
 
 go 1.13
+
+require (
+	github.com/gorilla/mux v1.8.0
+	github.com/prometheus/common v0.14.0
+)
--- main.go
+++ main.go
@@ -1,7 +1,36 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
 
 func main() {
-	fmt.Println("test")
+
+	router := mux.NewRouter()
+
+	router.HandleFunc("/signup", signup).Methods("POST")
+	router.HandleFunc("/login", login).Methods("POST")
+	router.HandleFunc("/protected", TokenVerifyMiddleWare(protectedEndpoint)).Methods("GET")
+
+	log.Println("Starting server on port 8080")
+	log.Fatal(http.ListenAndServe(":8080", router))
+}
+
+func signup(w http.ResponseWriter, r *http.Request) {
+	log.Println("signup")
+}
+
+func login(w http.ResponseWriter, r *http.Request) {
+	log.Println("login")
+}
+
+func protectedEndpoint(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func TokenVerifyMiddleWare(next http.HandlerFunc) http.HandlerFunc {
+	return nil
 }
